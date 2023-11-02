6b9a3fa2ba6f06b020243b35d58cf76244294f29
Carlitos <carlitosdutan@pursuit.org>
2023-11-02 14:48:25 -0400

    errorcode

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,65 @@ const removeButtons = document.querySelectorAll(".remove-button");
 
 removeButtons.forEach((removeButton) => {
     removeButton.addEventListener("click", (event) => {
-        console.log(removeButton);
         removeButton.closest("article").remove();
     })
+})
+
+
+// Create Cellphone
+
+// Grab Elements
+
+const phoneName = document.getElementById
+("phone-name");
+
+const imageUrl = document.getElementById
+("url");
+
+const brandName = document.getElementById
+("brand");
+
+const phoneColor = document.getElementById
+("color");
+
+const storage = document.getElementById
+("storage");
+
+const price = document.getElementById
+("price");
+
+const stock = document.getElementById
+("stock-select");
+
+
+// Create template
+
+const content = 
+`
+<p><span>Name: </span>${phoneName.value}</p>
+<p><span>Brand: </span>${brandName.value}</p>
+<p><span>Color: </span>${phoneColor.value}</p>
+<p><span>Storage: </span>${storage.value}</p>
+<p><span>Price: </span>${price.value}</p>
+<button class="stock-button">In Stock</button>
+<button class="remove-button">Remove</button>
+`
+
+// const createCellPhoneTemplate = content.innerHTML;
+
+// console.log(createCellPhoneTemplate);
+
+
+
+
+
+
+// Create Cellphone Button Functionality
+
+const createCellphoneButton = document.querySelector("form");
+
+createCellphoneButton.addEventListener("click", (event) => {
+    // const newCellphone = `${phoneName.value} ${brandName.value} ${phoneColor.value} ${storage.value} ${price.value},`
+    console.log(event.target.brand.value);
+    console.log("hello");
 })
